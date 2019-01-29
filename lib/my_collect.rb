e439552552821d6cb19f88c4f0511e33f6879dd3
CHRISTOPHER TRUONG <christophertruongm@gmail.com>
2019-01-29 03:05:51 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,14 +1,24 @@
 def my_collect(arr)
   i = 0
+<<<<<<< HEAD
   val = []
   
   while i < arr.length
     
    val <<  yield(arr[i])
+=======
+  
+  while i < arr.length
+    yield(arr[i])
+>>>>>>> 6dbac35b97a0f5cca88b6818081fd13c1c6c53c3
     
     i += 1
   end
   
+<<<<<<< HEAD
   val
+=======
+  return my_collect(arr)
+>>>>>>> 6dbac35b97a0f5cca88b6818081fd13c1c6c53c3
 end
 
